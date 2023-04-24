e372956a16711fdbb427a505ff8183513a122277
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-24 21:16:37 +0200

    atualizações

--- tv/index.php
+++ tv/index.php
@@ -4,9 +4,26 @@
 <head>
 	<title>SISTEMA - SCW</title>
 	<?php include('../lib/header.php'); ?>
+	<style>
+		.topoTV{
+			position:fixed;
+			left:0;
+			top:0;
+			right:0;
+			height:60px;
+		}
+	</style>
 </head>
 <body>
 <div id="app">Aplicação na página</div>
+
+<div class="topoTV d-flex justify-content-between">
+	<div>posicao 1</div>
+	<div>posicao 2</div>
+	<div>posicao 3</div>
+</div>
+
+
 <script type="text/javascript">
 
 	Carregando = (opc) => { $("#Carregando").css("display",(opc?opc:'block')) }
