00a51abf94c85c4f9556a0d9d3ea1bd607ef9f54
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-24 17:44:08 +0200

    atualizações

--- src/notificacoes/form.php
+++ src/notificacoes/form.php
@@ -81,18 +81,10 @@
     ?>
     <div class="card">
       <div class="card-header" id="heading<?=$s->codigo?>">
-        <h2 class="mb-0">
-
-
           <div class="form-group form-check" data-toggle="collapse" data-target="#collapse<?=$s->codigo?>" aria-expanded="true" aria-controls="collapse<?=$s->codigo?>">
             <input type="checkbox" class="form-check-input" id="time<?=$s->codigo?>">
             <label class="form-check-label" for="time<?=$s->codigo?>"><?=utf8_encode($s->nome)?></label>
           </div>
-
-          <!-- <button class="btn btn-link btn-secondary text-left" type="button" data-toggle="collapse" data-target="#collapse<?=$s->codigo?>" aria-expanded="true" aria-controls="collapse<?=$s->codigo?>">
-            <?=utf8_encode($s->nome)?>
-          </button> -->
-        </h2>
       </div>
 
       <div id="collapse<?=$s->codigo?>" class="collapse" aria-labelledby="heading<?=$s->codigo?>" data-parent="#accordionTime">
