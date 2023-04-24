6be659657f1cc9829e3f02a5570d9f0d2e9546c8
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-24 21:42:08 +0200

    atualizações

--- tv/index.php
+++ tv/index.php
@@ -41,24 +41,34 @@
 </div>
 <div class="corpoTV">
 
-		<div class="slider-for">
-			<h1>1</h1>
-			<h1>2</h1>
-			<h1>3</h1>
-			<h1>4</h1>
-			<h1>5</h1>
-		</div>
+	<div class="slider-for">
+		<h1>1</h1>
+		<h1>2</h1>
+		<h1>3</h1>
+		<h1>4</h1>
+		<h1>5</h1>
+		<h1>6</h1>
+		<h1>7</h1>
+		<h1>8</h1>
+		<h1>9</h1>
+		<h1>10</h1>
+	</div>
 
 </div>
 <div class="rodapeTV">
 
-		<div class="slider-nav">
-			<h1>1</h1>
-			<h1>2</h1>
-			<h1>3</h1>
-			<h1>4</h1>
-			<h1>5</h1>
-		</div>
+	<div class="slider-nav">
+		<h1>1</h1>
+		<h1>2</h1>
+		<h1>3</h1>
+		<h1>4</h1>
+		<h1>5</h1>
+		<h1>6</h1>
+		<h1>7</h1>
+		<h1>8</h1>
+		<h1>9</h1>
+		<h1>10</h1>
+	</div>
 
 </div>
 <script type="text/javascript">
