47330337f5e296f0ae4d550c105fb817c66ac944
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-24 17:30:15 +0200

    atualizações

--- src/notificacoes/form.php
+++ src/notificacoes/form.php
@@ -73,51 +73,31 @@
 
 
 
-  <div class="accordion" id="accordionExample">
-  <div class="card">
-    <div class="card-header" id="headingOne">
-      <h2 class="mb-0">
-        <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Collapsible Group Item #1
-        </button>
-      </h2>
-    </div>
+  <div class="accordion" id="accordionTime">
+    <?php
+      $q = "select * from time order by nome";
+      $r = mysql_query($q);
+      while($s = mysql_fetch_object($r)){
+    ?>
+    <div class="card">
+      <div class="card-header" id="heading<?=$s->codigo?>">
+        <h2 class="mb-0">
+          <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse<?=$s->codigo?>" aria-expanded="true" aria-controls="collapse<?=$s->codigo?>">
+            <?=$s->nome?>
+          </button>
+        </h2>
+      </div>
 
-    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-      <div class="card-body">
-        Some placeholder content for the first accordion panel. This panel is shown by default, thanks to the <code>.show</code> class.
+      <div id="collapse<?=$s->codigo?>" class="collapse show" aria-labelledby="heading<?=$s->codigo?>" data-parent="#accordionTme">
+        <div class="card-body">
+          Some placeholder content for the first accordion panel. This panel is shown by default, thanks to the <code>.show</code> class.
+        </div>
       </div>
     </div>
+    <?php
+      }
+    ?>
   </div>
-  <div class="card">
-    <div class="card-header" id="headingTwo">
-      <h2 class="mb-0">
-        <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          Collapsible Group Item #2
-        </button>
-      </h2>
-    </div>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-      <div class="card-body">
-        Some placeholder content for the second accordion panel. This panel is hidden by default.
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header" id="headingThree">
-      <h2 class="mb-0">
-        <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Collapsible Group Item #3
-        </button>
-      </h2>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-      <div class="card-body">
-        And lastly, the placeholder content for the third and final accordion panel. This panel is hidden by default.
-      </div>
-    </div>
-  </div>
-</div>
 
 
 
