3a8b70ac0e55ae42c507637ca6b823017572873f
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-24 21:51:36 +0200

    atualizações

--- tv/index.php
+++ tv/index.php
@@ -1,4 +1,60 @@
-<?php $home = true; include('../includes/includes.php'); ?>
+<?php
+	$home = true;
+	include('../includes/includes.php');
+
+	$q = "SELECT
+	a.codigo,
+	a.status,
+	a.time,
+	a.motivo,
+	tm.nome as time_nome,
+	mt.nome as motivo_nome,
+	s.nome as setor,
+	m.nome as maquina,
+	t.nome as tipo_manutencao,
+	a.problema,
+	f.nome as funcionario,
+	tc.nome as tecnico
+		FROM chamados a
+		left join setores s on a.setor = s.codigo
+		left join tipos_manutencao t on a.tipo_manutencao = t.codigo
+		left join maquinas m on a.maquina = m.codigo
+		left join time tm on a.time = tm.codigo
+		left join motivos mt on a.motivo = mt.codigo
+		left join login tc on a.tecnico = tc.codigo
+		left join login f on a.funcionario = f.codigo
+		order by desc limit 20";
+	$r = mysql_query($q);
+
+	$TickDetalhe = [];
+	$TickResumo = [];
+
+	while($d = mysql_fetch_object($r)){
+
+        $TickDetalhe[] = "<b>Cadastrado ID</b>: ".str_pad($d->codigo, 8, "0", STR_PAD_LEFT).
+               "<br> <b>SETOR</b>: ".utf8_encode($d->setor).
+               "<br> <b>MÁQUINA</b>: ".utf8_encode($d->maquina).
+               "<br> <b>TIPO DE MANUTENÇÃO</b>: ".utf8_encode($d->tipo_manutencao).
+               (($d->problema)?"<br> <b>PROBLEMA</b>: ".str_replace("\n"," ",utf8_encode($d->problema)):false).
+               (($d->funcionario)?"<br> <b>FUNCIONÁRIO</b>: ".utf8_encode($d->funcionario):false).
+               (($d->tecnico)?"<br> <b>TÉCNICO</b>: ".utf8_encode($d->tecnico):false).
+
+               (($d->time_nome)?"<br> <b>TIME</b>: ".utf8_encode($d->time_nome):false).
+               (($d->motivo_nome)?"<br> <b>MOTIVO</b>: ".utf8_encode($d->motivo_nome):false).
+
+
+               (($d->status)?"<br> <b>SITUAÇÃO</b>: ".$titulo[$d->status]:false).
+               (($d->observacao)?"<br> <b>OBSERVAÇÕES</b>: ".str_replace("\n"," ",$_POST['observacao']):false);
+
+		$TickResumo[] = "<b>Cadastrado ID</b>: ".str_pad($d->codigo, 8, "0", STR_PAD_LEFT).
+               "<br> <b>SETOR</b>: ".utf8_encode($d->setor).
+               "<br> <b>MÁQUINA</b>: ".utf8_encode($d->maquina);
+
+
+
+	}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -42,32 +98,26 @@
 <div class="corpoTV">
 
 	<div class="slider-for">
-		<h1>1</h1>
-		<h1>2</h1>
-		<h1>3</h1>
-		<h1>4</h1>
-		<h1>5</h1>
-		<h1>6</h1>
-		<h1>7</h1>
-		<h1>8</h1>
-		<h1>9</h1>
-		<h1>10</h1>
+		<?php
+		for($i=0;$i<count($TickDetalhe);$i++){
+		?>
+		<div><?=$TickDetalhe[$i]?></div>
+		<?php
+		}
+		?>
 	</div>
 
 </div>
 <div class="rodapeTV">
 
 	<div class="slider-nav">
-		<h1>1</h1>
-		<h1>2</h1>
-		<h1>3</h1>
-		<h1>4</h1>
-		<h1>5</h1>
-		<h1>6</h1>
-		<h1>7</h1>
-		<h1>8</h1>
-		<h1>9</h1>
-		<h1>10</h1>
+		<?php
+		for($i=0;$i<count($TickResumo);$i++){
+		?>
+		<div><?=$TickResumo[$i]?></div>
+		<?php
+		}
+		?>
 	</div>
 
 </div>
