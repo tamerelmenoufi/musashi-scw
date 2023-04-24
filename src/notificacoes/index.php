d506db3bb7bbb129597dfc0b24994c4a64292286
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-24 19:00:44 +0200

    atualizações

--- src/notificacoes/index.php
+++ src/notificacoes/index.php
@@ -44,7 +44,7 @@
 
           $time = false;
           if($d->time){
-            $q = "select * from time where codigo in({$d->time})";
+           echo $q = "select * from time where codigo in({$d->time})";
             $r = mysql_query($q);
             while($d1 = mysql_fetch_object($r)){
               $time = "$d1->nome<br>";
