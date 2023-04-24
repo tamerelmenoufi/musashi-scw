20fa1aa580497ddc7b09afde4a810a775c0bb0e2
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-24 21:01:31 +0200

    atualizações

---
+++ tv/index.php
@@ -0,0 +1,18 @@
+<?php $home = true; include('../includes/includes.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SISTEMA - SCW</title>
+	<?php include('../lib/header.php'); ?>
+</head>
+<body>
+<div id="app">
+	Aplicação na página
+</div>
+<script type="text/javascript">
+	$(function(){
+
+	})
+</script>
+</body>
+</html>
