a8f947b7b3c66d12f713762278e8f0acc21c9337
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-24 21:18:30 +0200

    atualizações

--- tv/index.php
+++ tv/index.php
@@ -11,18 +11,27 @@
 			top:0;
 			right:0;
 			height:60px;
+			border:solid 1px red;
+		}
+		.corpoTV{
+			position:fixed;
+			left:0;
+			top:60px;
+			right:0;
+			height:60px;
+			border:solid 1px red;
 		}
 	</style>
 </head>
 <body>
-<div id="app">Aplicação na página</div>
+
 
 <div class="topoTV d-flex justify-content-between">
 	<div>posicao 1</div>
 	<div>posicao 2</div>
 	<div>posicao 3</div>
 </div>
-
+<div id="corpoTV">Aplicação na página</div>
 
 <script type="text/javascript">
 
