1942ee9b4493874ed2b904c2a65df54e6ec8b3bf
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-25 18:57:27 +0200

    atualizações

--- tv/index.php
+++ tv/index.php
@@ -51,7 +51,7 @@
         $TickDetalhe[] = "
 				<div style='float:left; width:33%;'><b style='color:#a1a1a1; font-size:10px;''>Cadastrado ID:</b> <div>".str_pad($d->codigo, 8, "0", STR_PAD_LEFT)."</div></div>".
 				"<div style='float:left; width:33%;'> <b style='color:#a1a1a1; font-size:10px;'>Máquina:</b><div>".utf8_encode($d->maquina)."</div></div>".
-				"<div style='float:left; width:33%;'>".(($d->observacao)?"<b style='color:#a1a1a1; font-size:10px;'>Observações:</b><div>".str_replace("\n"," ",$_POST['observacao'])."</div>":false)."</div>".
+				"<div style='float:left; width:33%;'>".(($d->status)?"<b style='color:#a1a1a1; font-size:10px;'>Situação:</b><div>".$titulo[$d->status]."</div>":false)."</div>".
 
 				"<div> <b style='color:#a1a1a1; font-size:10px;'>Setor:</b><div>".utf8_encode($d->setor)."</div></div>".
 
@@ -63,8 +63,7 @@
                "<div>".(($d->time_nome)?"<b style='color:#a1a1a1; font-size:10px;'>Time:</b><div>".utf8_encode($d->time_nome)."</div>":false)."</div>".
                "<div>".(($d->motivo_nome)?"<b style='color:#a1a1a1; font-size:10px;'>Motivo:</b><div>".utf8_encode($d->motivo_nome)."</div>":false)."</div>".
 
-
-               "<div>".(($d->status)?"<b style='color:#a1a1a1; font-size:10px;'>Situação:</b><div>".$titulo[$d->status]."</div>":false)."</div>";
+               "<div>".(($d->observacao)?"<b style='color:#a1a1a1; font-size:10px;'>Observações:</b><div>".str_replace("\n"," ",$_POST['observacao'])."</div>":false)."</div>";
 
 		$TickResumo[] = "<b>Cadastrado ID</b>: ".str_pad($d->codigo, 8, "0", STR_PAD_LEFT).
                "<br> <b>SETOR</b>: ".utf8_encode($d->setor).
