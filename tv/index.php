38bcfc4a7a8ddba1ecbe8cce6fd4ceddbe03f964
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-25 19:53:44 +0200

    atualizações

--- tv/index.php
+++ tv/index.php
@@ -16,6 +16,11 @@
 		'c' => 'green',
   	);
 
+	$parada = array(
+		's' => 'Máquina Parada',
+		'n' => 'Máquina Funcionando',
+  	);
+
 	$q = "SELECT
 	a.codigo,
 	a.data_abertura,
@@ -53,7 +58,7 @@
 				<div style='float:left; width:33%;'><b style='color:#a1a1a1; font-size:10px;''>Cadastrado ID:</b> <div>".str_pad($d->codigo, 8, "0", STR_PAD_LEFT)."</div></div>".
 				"<div style='float:left; width:33%;'>".((dataBr($d->data_abertura))?"<b style='color:#a1a1a1; font-size:10px;'>Data:</b><div>".dataBr($d->data_abertura)."</div>":false)."</div>".
 				"<div style='float:left; width:33%;'>".(($d->status)?"<b style='color:#a1a1a1; font-size:10px;'>Situação:</b>
-					<div style='color:{$cor[$d->status]}; font-weight:bold;'>".$titulo[$d->status]."<span style='color:".(($d->status == 's')?'red':'green')."; font-weight:bold;'> (".$titulo[$d->status].")</span></div>":false)."</div>".
+					<div style='color:{$cor[$d->status]}; font-weight:bold;'>".$titulo[$d->status]."<span style='color:".(($d->status == 's')?'red':'green')."; font-weight:bold;'> (".$parada[$d->parada].")</span></div>":false)."</div>".
 
 				"<div style='float:left; width:50%;'> <b style='color:#a1a1a1; font-size:10px;'>Setor:</b><div>".utf8_encode($d->setor)."</div></div>".
 				"<div style='float:left; width:50%;'> <b style='color:#a1a1a1; font-size:10px;'>Máquina:</b><div>".utf8_encode($d->maquina)."</div></div>".
