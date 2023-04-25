65f8765af917c5373863f0518f12224ba974bcff
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-25 16:36:49 +0200

    atualizações

--- tv/index.php
+++ tv/index.php
@@ -130,7 +130,7 @@
 		<img src="img/logo.png" style="height:50px; margin:5px;">
 	</div>
 	<div>
-		<div style="background-color:rgb(255,255,255,0.3); border-radius:5px; padding:2px; margin-top:20px;">
+		<div style="background-color:rgb(255,255,255,0.3); border-radius:5px; padding:2px; margin-top:15px;">
 			<i class="fa fa-square" style="color:green;"></i> Atendido
 			<i class="fa fa-square" style="color:orange;"></i> Em Andamento
 			<i class="fa fa-square" style="color:red;"></i> Novo
