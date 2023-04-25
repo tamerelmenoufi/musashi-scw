900df9e5db059c01863017497433829f4b9b8d25
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-25 22:48:01 +0200

    atualizações

--- tv/index.php
+++ tv/index.php
@@ -58,7 +58,7 @@
 	while($d = mysql_fetch_object($r)){
 
 
-		echo $d->status ." : ". $Qt['novos'] = (($d->status == 'n')?($Qt['novos'] = ($Qt['novos'] + 1)):($Qt['novos']))."<br>";
+		$Qt['novos'] = (($d->status == 'n')?($Qt['novos'] = ($Qt['novos'] + 1)):($Qt['novos']))."<br>";
 		$Qt['pendentes'] = (($d->status == 'p')?($Qt['pendentes'] = ($Qt['pendentes'] + 1)):($Qt['pendentes']));
 		$Qt['concluidos'] = (($d->status == 'c')?($Qt['concluidos'] = ($Qt['concluidos'] + 1)):($Qt['concluidos']));
 		$Qt['parados'] = (($d->parada == 's')?($Qt['parados'] = ($Qt['parados'] + 1)):($Qt['parados']));
