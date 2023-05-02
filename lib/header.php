dda1265e67f655bdb309ae4847ba917ad63daf35
tamerelmenoufi <tamer@mohatron.com.br>
2023-05-02 15:31:50 +0300

    atualizações

--- lib/header.php
+++ lib/header.php
@@ -92,6 +92,7 @@
 
 			<script type="text/javascript" src="<?=$UrlProjeto?>/lib/v1/js/js.js"></script>
 
+			<script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/js/bootstrap-select.min.js"></script>
 			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/css/bootstrap-select.min.css">
 
 
