b471f8690f1432efa39134b4c486696a58196a84
tamerelmenoufi <tamer@mohatron.com.br>
2023-05-02 20:29:41 +0300

    atualizações

--- lib/header.php
+++ lib/header.php
@@ -104,7 +104,7 @@
 <!-- Scripts -->
 <!-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.0/dist/jquery.slim.min.js"></script> -->
 <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script> -->
-<!-- <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script> -->
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
 			<style>
 				#Carregando{
