fc7b6a0317f705ac8f2f00f7d94b273f5d87e96d
tamerelmenoufi <tamer@mohatron.com.br>
2023-05-05 00:02:51 +0300

    atualizações

--- chamados.php
+++ chamados.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
     //$home = true;
     include('includes/includes.php');
 
@@ -12,9 +12,9 @@
     function Funcao_utf8($dado){
         return $dado;
     }
-    
+
     function status($st){
-        
+
         if($st == 'n'){
             return utf8_decode('Novo');
         }elseif($st == 'p'){
@@ -22,29 +22,70 @@
         }elseif($st == 'c'){
             return utf8_decode('Concluído');
         }
-        
-        
+
+
     }
 
-    $query = "SELECT 
+    $query = "SELECT
             		a.*,
                     b.nome as funcionario,
                     c.nome as setor,
                     d.nome as tipo_manutencao,
                     e.nome as maquina,
                     f.nome as tecnico
-            
-            
-            FROM  chamados a 
-            
+
+
+            FROM  chamados a
+
             left join login b on a.funcionario = b.codigo
             left join setores c on a.setor = c.codigo
             left join tipos_manutencao d on a.tipo_manutencao = d.codigo
             left join maquinas e on a.maquina = e.codigo
             left join login f on a.tecnico = f.codigo order by data_abertura desc limit 1000";
-    
+
+
+    $query = "SELECT
+                    a.codigo,
+                    a.status,
+                    a.time,
+                    a.motivo,
+                    a.parada,
+
+                    a.peca,
+                    a.modelo,
+                    a.codigos,
+
+                    tm.nome as time_nome,
+                    mt.nome as motivo_nome,
+                    s.nome as setor,
+                    m.nome as maquina,
+
+                    p.nome as peca_nome,
+                    md.nome as modelo_nome,
+                    cd.nome as codigo_nome,
+
+                    t.nome as tipo_manutencao,
+                    a.problema,
+                    f.nome as funcionario,
+                    tc.nome as tecnico
+            FROM chamados a
+            left join setores s on a.setor = s.codigo
+
+            left join pecas p on a.peca = p.codigo
+            left join modelos md on a.modelo = md.codigo
+            left join codigos cd on a.codigos = cd.codigo
+
+            left join tipos_manutencao t on a.tipo_manutencao = t.codigo
+            left join maquinas m on a.maquina = m.codigo
+            left join time tm on a.time = tm.codigo
+            left join motivos mt on a.motivo = mt.codigo
+            left join login tc on a.tecnico = tc.codigo
+            left join login f on a.funcionario = f.codigo
+            order by data_abertura desc limit 1000";
+
+
     $result = mysql_query($query);
-?>    
+?>
 <table>
     <thead>
         <tr>
@@ -53,8 +94,16 @@
             <th><?=utf8_decode('DATA DE RECEBIMENTO')?></th>
             <th><?=utf8_decode('DATA DE FECHAMENTO')?></th>
             <th><?=utf8_decode('SETOR')?></th>
-            <th><?=utf8_decode('TIPO DA MANUTENÇÃO')?></th>
+
+            <th><?=utf8_decode('TIME DE ATUAÇÃO')?></th>
+            <th><?=utf8_decode('OCORRÊNCIA')?></th>
+
             <th><?=utf8_decode('MÁQUINA')?></th>
+            <th><?=utf8_decode('PEÇA')?></th>
+            <th><?=utf8_decode('MODELO')?></th>
+            <th><?=utf8_decode('CÓDIGO')?></th>
+
+
             <th><?=utf8_decode('TÉCNICO')?></th>
             <th><?=utf8_decode('PROBLEMA')?></th>
             <th><?=utf8_decode('SITUAÇÃO')?></th>
@@ -62,34 +111,42 @@
         </tr>
     </thead>
     <tbody>
-<?php    
+<?php
     while($d = mysql_fetch_object($result)){
-        
+
         $q = "SELECT b.nome, a.data, a.observacao FROM chamados_observacoes a left join login b on a.tecnico = b.codigo where chamado = '".$d->codigo."'";
         $r = mysql_query($q);
         $obs_tecnicos = array();
         while($d1 = mysql_fetch_object($r)){
-            $obs_tecnicos[] = dataBr($d1->nome).' - '.Funcao_utf8($d1->nome).": ".Funcao_utf8($d1->observacao);   
+            $obs_tecnicos[] = dataBr($d1->nome).' - '.Funcao_utf8($d1->nome).": ".Funcao_utf8($d1->observacao);
         }
-        
+
         $obs_tecnicos = @implode('; ',$obs_tecnicos);
-        
-        
-?>        
+
+
+?>
         <tr>
             <td><?=Funcao_utf8($d->funcionario)?></td>
             <td><?=dataBr($d->data_abertura)?></td>
             <td><?=dataBr($d->data_recebimento)?></td>
             <td><?=dataBr($d->data_fechamento)?></td>
             <td><?=Funcao_utf8($d->setor)?></td>
-            <td><?=Funcao_utf8($d->tipo_manutencao)?></td>
+
+            <td><?=Funcao_utf8($d->time_nome)?></td>
+            <td><?=Funcao_utf8($d->motivo_nome)?></td>
+
             <td><?=Funcao_utf8($d->maquina)?></td>
+            <td><?=Funcao_utf8($d->peca_nome)?></td>
+            <td><?=Funcao_utf8($d->modelo_nome)?></td>
+            <td><?=Funcao_utf8($d->codigo_nome)?></td>
+
+
             <td><?=Funcao_utf8($d->tecnico)?></td>
             <td><?=Funcao_utf8($d->problema)?></td>
             <td><?=status($d->status)?></td>
             <td><?=$obs_tecnicos?></td>
         </tr>
-<?php        
+<?php
     }
 ?>
     </tbody>
