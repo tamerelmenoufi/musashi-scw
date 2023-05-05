8fe98b46b4307af2fd9e65e71c75101143b3f74d
tamerelmenoufi <tamer@mohatron.com.br>
2023-05-06 01:05:19 +0300

    atualizações

--- tv/index.php
+++ tv/index.php
@@ -537,12 +537,12 @@
 		$('.slider-for').slick({
 			slidesToShow: 1,
 			slidesToScroll: 1,
-			arrows: true,
+			arrows: false,
 			fade: true,
 			asNavFor: '.slider-nav'
 		});
 		$('.slider-nav').slick({
-			slidesToShow: 5,
+			slidesToShow: 1,
 			slidesToScroll: 1,
 			asNavFor: '.slider-for',
 			dots: false,
