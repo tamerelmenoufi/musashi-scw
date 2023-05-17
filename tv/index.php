7e57add6682385331c81e791d00a2a91b8b5efe6
tamerelmenoufi <tamer@mohatron.com.br>
2023-05-17 16:16:59 +0300

    Atualizações

--- tv/index.php
+++ tv/index.php
@@ -167,12 +167,12 @@
 
                "<div style='width:100%;'>".(($d->observacao)?"<b style='color:#a1a1a1; font-size:10px;'>Observações:</b><div class='detalhesTexto'>".str_replace("\n"," ",$_POST['observacao'])."</div>":false)."</div><br>";
 
-		$TickResumo[] = "<div style='float:left; width:40%;'><b style='font-size:10px;''>Cadastrado ID:</b> <div>".str_pad($d->codigo, 8, "0", STR_PAD_LEFT)."</div></div>".
-						"<div style='float:left; width:60%;'>".(($d->status)?"<b style='font-size:10px;'>Situação:</b><div>".$titulo[$d->status]."</div>":false)."</div>".
+		$TickResumo[] = "<div style='float:left; width:40%;'><b style='font-size:20px;''>Cadastrado ID:</b> <div>".str_pad($d->codigo, 8, "0", STR_PAD_LEFT)."</div></div>".
+						"<div style='float:left; width:60%;'>".(($d->status)?"<b style='font-size:20px;'>Situação:</b><div>".$titulo[$d->status]."</div>":false)."</div>".
 						"<div > <b style='font-size:15px;'>Setor:</b><div>".utf8_encode($d->setor_nome)."</div></div>".
-						"<div> <b style='font-size:15px;'>Máquina: (".$parada[$d->parada].")</b><div>".utf8_encode($d->maquina_nome)."</div></div>".
-						"<div > <b style='font-size:15px;'>Time:</b><div>".utf8_encode($d->time_nome)."</div></div>".
-						"<div> <b style='font-size:15px;'>Ocorrência:</b><div>".utf8_encode($d->motivo_nome)."</div></div>";
+						"<div> <b style='font-size:20px;'>Máquina: (".$parada[$d->parada].")</b><div>".utf8_encode($d->maquina_nome)."</div></div>".
+						"<div > <b style='font-size:20px;'>Time:</b><div>".utf8_encode($d->time_nome)."</div></div>".
+						"<div> <b style='font-size:20px;'>Ocorrência:</b><div>".utf8_encode($d->motivo_nome)."</div></div>";
 
 
 
