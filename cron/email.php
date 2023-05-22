819301dc6ee4164bf984f647b75d0ba7b6937e07
tamerelmenoufi <tamer@mohatron.com.br>
2023-05-22 22:24:23 +0300

    Atualizações

--- cron/email.php
+++ cron/email.php
@@ -32,7 +32,7 @@ function EnviaEmailNovo($codigo, $time){
             $json = file_get_contents($path.$file);
             $send = json_decode($json);
             $data = print_r($send, true);
-            // file_put_contents('result.txt', $path.$file." || ".$data. " || ".date("d/m/Y H:i:s")."\n\n\n\n", FILE_APPEND | LOCK_EX);
+            file_put_contents('result.txt', $path.$file." || ".$data. " || ".date("d/m/Y H:i:s")."\n\n\n\n", FILE_APPEND | LOCK_EX);
             unlink($path.$file);
             EnviaEmailNovo($send->email, $send->time);
             // echo $file."\n";
