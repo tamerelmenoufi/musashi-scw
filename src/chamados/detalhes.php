a27aabb6a59e0d631faaafd73b35ce73f21f67e8
tamerelmenoufi <tamer@mohatron.com.br>
2023-05-30 16:18:11 +0300

    Atualizações

--- src/chamados/detalhes.php
+++ src/chamados/detalhes.php
@@ -231,7 +231,7 @@
     	<span><?=dataBr($d->data_recebimento)?></span>
 
     <?php
-    if($d->status == 'p' /*and $d->tecnico_codigo == $_SESSION['scw_usuario_logado']*/){
+    if($d->status == 'p' and $d->tecnico_codigo == $_SESSION['scw_usuario_logado']){
     ?>
 		<div class="form-group">
 			<label for="observacoes">Incluir Observações</label>
