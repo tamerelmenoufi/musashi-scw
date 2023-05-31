7844e49b3983e0d40ea088e09691ef3c766a2738
tamerelmenoufi <tamer@mohatron.com.br>
2023-05-31 21:39:35 +0300

    Atualizações

---
+++ app/update.php
@@ -0,0 +1,13 @@
+<?php
+	$home = true;
+	include('../includes/includes.php');
+
+	$q = "SELECT count(*) as qt, max(UNIX_TIMESTAMP(data_atualizacao)) as tempo FROM `chamados`";
+	$r = mysql_query($q);
+    $d = mysql_fetch_object($r);
+
+    $json = [
+        'status' => (($d->qt != $_POST['qt'] or $d->tempo != $_POST['tempo'])?true:false),
+    ];
+
+    echo json_encode($json);
