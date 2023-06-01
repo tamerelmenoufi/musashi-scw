acc2756d79d40971551656006822af136893ff40
tamerelmenoufi <tamer@mohatron.com.br>
2023-06-01 16:41:14 +0300

    Atualizações

--- app/index.php
+++ app/index.php
@@ -338,7 +338,7 @@
 		<img src="img/logo.png" style="height:30px; margin:5px;">
 	</div>
 	<div>
-		<div style="background-color:rgb(255,255,255,0.3); border-radius:5px; padding:2px; margin-top:15px; font-size:10px;">
+		<div style="background-color:rgb(255,255,255,0.3); border-radius:5px; padding:2px; margin-top:5px; font-size:10px;">
 			<i class="fa fa-square" style="color:blue;"></i> Novo
 			<i class="fa fa-square" style="color:orange; margin-left:10px;"></i> Pendente
 			<i class="fa fa-square" style="color:green; margin-left:10px;"></i> Concluído
