0201f59e26577bd93b75af278b3f8e934f70f843
tamerelmenoufi <tamer@mohatron.com.br>
2023-06-01 16:38:06 +0300

    Atualizações

--- app/index.php
+++ app/index.php
@@ -335,7 +335,7 @@
 
 <div class="topoTV d-flex justify-content-between">
 	<div>
-		<img src="img/logo.png" style="height:50px; margin:5px;">
+		<img src="img/logo.png" style="height:30px; margin:5px;">
 	</div>
 	<div>
 		<div style="background-color:rgb(255,255,255,0.3); border-radius:5px; padding:2px; margin-top:15px; font-size:10px;">
