4c6f3bb0a2b442e131e3c28dcec7b26528b8929a
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-30 01:30:31 +0200

    atualizações

--- src/relatorios/index.php
+++ src/relatorios/index.php
@@ -223,23 +223,26 @@ $j++;
     ///////////////////////// Chamados ////////////////////////////////////////////////////////////
 
 
-    // const xValues = [100,200,300,400,500,600,700,800,900,1000];
+    const xValues = [100,200,300,400,500,600,700,800,900,1000];
 
     new Chart("grafico_utm", {
         type: "horizontalBar",
         data: {
-            // labels: xValues,
+            labels: xValues,
             datasets: [{
             data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
             borderColor: "red",
+            backgroundColor:"red",
             fill: false
             },{
             data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
             borderColor: "green",
+            backgroundColor:"red",
             fill: false
             },{
             data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
             borderColor: "blue",
+            backgroundColor:"red",
             fill: false
             }]
         },
