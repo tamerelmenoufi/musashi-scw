e8904b1d2339b3971d884f7288eff7bfec57314b
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-30 01:47:03 +0200

    atualizações

--- src/relatorios/index.php
+++ src/relatorios/index.php
@@ -233,32 +233,32 @@ $j++;
             datasets: [{
             label: 'Geral',
             data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
-            borderColor: "red",
-            backgroundColor:"red",
+            borderColor: "blue",
+            backgroundColor:"blue",
             fill: false
             },{
             label: 'Concluido',
             data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
             borderColor: "green",
-            backgroundColor:"red",
+            backgroundColor:"green",
             fill: false
             },{
             label: 'Pendente',
             data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
-            borderColor: "blue",
-            backgroundColor:"red",
+            borderColor: "gray",
+            backgroundColor:"gray",
             fill: false
             },{
             label: 'Paradas',
             data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
-            borderColor: "blue",
+            borderColor: "red",
             backgroundColor:"red",
             fill: false
             },{
             label: 'Em Produção',
             data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
-            borderColor: "blue",
-            backgroundColor:"red",
+            borderColor: "orange",
+            backgroundColor:"orange",
             fill: false
             }]
         },
