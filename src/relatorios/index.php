54fb0ed3e240c2f1f28d83fa832e01923c757f76
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-30 01:59:57 +0200

    atualizações

--- src/relatorios/index.php
+++ src/relatorios/index.php
@@ -234,31 +234,31 @@ $j++;
             label: 'Geral',
             data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
             borderColor: "blue",
-            backgroundColor:"rgb(2, 62, 198, 0.5)",
+            backgroundColor:"rgb(2, 62, 198, 0.7)",
             fill: false
             },{
             label: 'Concluido',
             data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
             borderColor: "green",
-            backgroundColor:"rgb(1, 174, 50, 0.5)",
+            backgroundColor:"rgb(1, 174, 50, 0.7)",
             fill: false
             },{
             label: 'Pendente',
             data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
             borderColor: "gray",
-            backgroundColor:"rgb(116, 116, 116, 0.5)",
+            backgroundColor:"rgb(116, 116, 116, 0.7)",
             fill: false
             },{
             label: 'Paradas',
             data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
             borderColor: "red",
-            backgroundColor:"rgb(200, 3, 54, 0.5)",
+            backgroundColor:"rgb(200, 3, 54, 0.7)",
             fill: false
             },{
             label: 'Em Produção',
             data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
             borderColor: "orange",
-            backgroundColor:"rgb(247, 152, 2, 0.5)",
+            backgroundColor:"rgb(247, 152, 2, 0.7)",
             fill: false
             }]
         },
