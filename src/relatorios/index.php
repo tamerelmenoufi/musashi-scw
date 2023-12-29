01f7b07615cfb83a6ec2fb3d0ee192d8c4add79c
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-29 23:31:24 +0200

    atualizações

--- src/relatorios/index.php
+++ src/relatorios/index.php
@@ -216,66 +216,41 @@
 
     ///////////////////////// Chamados ////////////////////////////////////////////////////////////
     var ctx = document.getElementById('grafico_utm').getContext('2d');
-    
+    const labels = Utils.months({count: 7});
+    const data = {
+    labels: labels,
+    datasets: [{
+        axis: 'y',
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)'
+        ],
+        borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)'
+        ],
+        borderWidth: 1
+    }]
+    };
     var chart = new Chart(ctx, {
         // The type of chart we want to create
         type: 'bar',
     
         // The data for our dataset
-        data: {
-            labels: [
-                <?php
-                    $grafico = [];
-                    foreach($relatorio['setor'] as $i => $v){
-                ?>
-                        '<?=$i?>',    
-                <?php
-                    }
-                ?>
-                ],
-            datasets: [
-                <?php
-                    $grafico = [];
-                    foreach($relatorio['setor'] as $i => $v){
-                ?>
-                [
-                {
-                    label: 'Geral',
-                    backgroundColor: 'blue',
-                    borderColor: 'blue',
-                    data: [10]
-                }, 
-                {
-                    label: 'Pendentes',
-                    backgroundColor: 'gray',
-                    borderColor: 'gray',
-                    data: [20]
-                }, 
-                {
-                    label: 'Concluidos',
-                    backgroundColor: 'green',
-                    borderColor: 'green',
-                    data: [30]
-                }, 
-                {
-                    label: 'Parada',
-                    backgroundColor: 'red',
-                    borderColor: 'red',
-                    data: [40]
-                },
-                {
-                    label: 'Em Produção',
-                    backgroundColor: 'orange',
-                    borderColor: 'orange',
-                    data: [50]
-                },    
-                ], 
-                <?php
-                    }
-                ?>
-                
-            ]
-        },
+        data: data,
     
         // Configuration options go here
         options: {
