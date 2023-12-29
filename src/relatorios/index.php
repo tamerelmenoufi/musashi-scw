325f8f7699937c4b11b9c8387460954a8d457926
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-30 01:36:26 +0200

    atualizações

--- src/relatorios/index.php
+++ src/relatorios/index.php
@@ -231,16 +231,19 @@ $j++;
         data: {
             labels: xValues,
             datasets: [{
+            labels: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
             data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
             borderColor: "red",
             backgroundColor:"red",
             fill: false
             },{
+            labels: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
             data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
             borderColor: "green",
             backgroundColor:"red",
             fill: false
             },{
+            labels: [300,700,2000,5000,6000,4000,2000,1000,200,100],
             data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
             borderColor: "blue",
             backgroundColor:"red",
