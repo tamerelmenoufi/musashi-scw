904f8f4142195cdff95ecb4c0f7f85df57cd6fbe
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-25 20:51:30 +0200

    Atualizações

--- modelo.php
+++ modelo.php
@@ -27,9 +27,9 @@
     while($d = mysql_fetch_object($result)){
 ?>
         <tr>
-            <td><?=utf8_decode($d->setor_nome)?></td>
-            <td><?=$d->time_nome?></td>
-            <td><?=$d->qt?></td>
+            <td><?=utf8_encode($d->setor_nome)?></td>
+            <td><?=utf8_encode($d->time_nome)?></td>
+            <td><?=utf8_encode($d->qt)?></td>
         </tr>
 <?php
     $i++;
