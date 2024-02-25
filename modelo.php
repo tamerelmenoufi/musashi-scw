ea12e40bbdebc2a999cd4cf9cd4f89d75347edd3
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-25 20:55:02 +0200

    Atualizações

--- modelo.php
+++ modelo.php
@@ -6,10 +6,12 @@
                     a.*,
                     s.nome as setor_nome,
                     t.nome as time_nome,
+                    u.nome as utm_nome,
                     count(*) as qt
                 from chamados a 
                     left join setores s on a.setor = s.codigo
                     left join time t on a.time = t.codigo
+                    left join utm u on s.utm = u.codigo
                 where a.status != 'c' group by a.setor, a.time";
     $result = mysql_query($query);
     $i = 1;
@@ -27,7 +29,7 @@
     while($d = mysql_fetch_object($result)){
 ?>
         <tr>
-            <td><?=utf8_encode($d->setor_nome)?></td>
+            <td><?=utf8_encode($d->setor_nome)?> - <?=utf8_encode($d->utm_nome)?></td>
             <td><?=utf8_encode($d->time_nome)?></td>
             <td><?=utf8_encode($d->qt)?></td>
         </tr>
