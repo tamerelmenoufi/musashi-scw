3f1f1098c14d7f8898a607ddd538d36c0bd0426e
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-25 20:47:54 +0200

    Atualizações

--- modelo.php
+++ modelo.php
@@ -2,13 +2,38 @@
     $home = true; 
     include('includes/includes.php');
 
-    $query = "select * from chamados where status != 'c' group by setor, time";
+    $query = "select 
+                    a.*,
+                    s.nome as setor_nome,
+                    t.nome as time_nome,
+                    count(*) as qt
+                from chamados a 
+                    left join setores s on a.setor = s.codigo
+                    left join time t on a.time = t.codigo
+                where a.status != 'c' group by a.setor, a.time";
     $result = mysql_query($query);
     $i = 1;
+?>
+<table>
+    <thead>
+        <tr>
+            <th>Setor</th>
+            <th>Time de Atuação</th>
+            <th>Quantidade</th>
+        </tr>
+    </thead>
+    <tbody>
+<?php
     while($d = mysql_fetch_object($result)){
 ?>
-<?=$d->problema?> - <?=$d->status?><br>
+        <tr>
+            <td><?=$d->setor_nome?></td>
+            <td><?=$d->time_nome?></td>
+            <td><?=$d->qt?></td>
+        </tr>
 <?php
     $i++;
     }
 ?>
+    </tbody>
+</table>
