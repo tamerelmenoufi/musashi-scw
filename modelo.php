4f1ed3588a11dbaf17c427f532bf7dbf798fc294
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-25 21:51:15 +0200

    Atualizações

--- modelo.php
+++ modelo.php
@@ -35,7 +35,7 @@
     while($d = mysql_fetch_object($result)){
 ?>
         <tr>
-            <td><?=utf8_encode($d->utm_nome)?></td>
+            <td><?=utf8_encode((($d->utm_nome)?:'Não Identificada'))?></td>
             <td><?=$d->novos?></td>
             <td><?=$d->pendentes?></td>
             <td><?=$d->concluidos?></td>
