dd81071131202cfac868983d6c47386682f5da22
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-25 20:31:32 +0200

    Atualizações

--- modelo.php
+++ modelo.php
@@ -3,7 +3,7 @@
     include('includes/includes.php');
 
     $query = "select * from chamados where status != 'c' order by data_abertura desc, status";
-    $result = mysql_query($result);
+    $result = mysql_query($query);
     $i = 1;
     while($d = mysql_fetch_object($result)){
 ?>
