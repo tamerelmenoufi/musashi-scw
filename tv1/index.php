93aa667d1073f5135a9741e09a2269b75cf8b1a9
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-29 13:40:11 +0200

    Atualizações

--- tv1/index.php
+++ tv1/index.php
@@ -373,7 +373,7 @@
 		<?php
 		for($i=0;$i<count($TickDetalhe);$i++){
 		?>
-		<div data-codigo="<?=$Codigo[$i]?>" class="listaDestaque" style="opacity:0.5; padding-top:5px; border-top:8px solid <?=$CorDetalhe[$i]?>; border-radius:10px;"><?=$TickDetalhe[$i]?></div>
+		<div data-codigo="<?=$Codigo[$i]?>" class="listaDestaque" style="opacity:0.5; padding-top:5px; border-top:8px solid <?=$CorDetalhe[$i]?>; border-radius:10px; height:50%;"><?=$TickDetalhe[$i]?></div>
 		<?php
 		}
 		?>
