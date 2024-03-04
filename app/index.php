43f22a0b2f3e02b8bac88a465589e3a35d1c4704
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-04 19:11:09 +0200

    Atualizações

--- app/index.php
+++ app/index.php
@@ -656,13 +656,13 @@
 </table>
 </div>
 <div class="row bg-warning">
-	<div class="col">
+	<div class="col-4">
 		<div style="width:100%; text-align:center; font-size:10px; font-weight:bold">NV (NOVO)</div>
 	</div>
-	<div class="col">
+	<div class="col-4">
 		<div style="width:100%; text-align:center; font-size:10px; font-weight:bold">PD (PENDENTE)</div>
 	</div>
-	<div class="col">
+	<div class="col-4">
 		<div style="width:100%; text-align:center; font-size:10px; font-weight:bold">CL (CONCLUÍDO)</div>
 	</div>
 </div>
