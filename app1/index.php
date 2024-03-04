11a359d4a895a61bb88343570da20180f7279a14
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-04 17:44:29 +0200

    Atualizações

--- app1/index.php
+++ app1/index.php
@@ -376,11 +376,11 @@
 	</div>
 	<div>
 		<div style="background-color:rgb(255,255,255,0.3); border-radius:5px; padding:2px; margin-top:10px; font-size:10px;">
-			<i class="fa fa-square" style="color:blue;"></i> Novo
+			<i class="fa fa-square" style="color:blue;"></i> Chamados
 			<i class="fa fa-square" style="color:orange; margin-left:10px;"></i> Pendente
+			<i class="fa fa-square" style="color:red; margin-left:10px;"></i> Paradas
 			<i class="fa fa-square" style="color:green; margin-left:10px;"></i> Concluído
-			<i class="fa fa-square" style="color:red; margin-left:10px;"></i> Máquina Parada
-			<i class="fa fa-square" style="color:yellow; margin-left:10px;"></i> Máquina Funcionando
+			<!-- <i class="fa fa-square" style="color:yellow; margin-left:10px;"></i> Máquina Funcionando -->
 		</div>
 	</div>
 	<div>
