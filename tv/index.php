b51639baafa6ab1c8cc6011b520a8b834cde3d1e
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-06 22:16:09 +0200

    Atualizações

--- tv/index.php
+++ tv/index.php
@@ -2,6 +2,25 @@
 	$home = true;
 	include('../includes/includes.php');
 
+	function periodo($dt){
+		$mes = [
+			'01'=>'jan',
+			'02'=>'fev',
+			'03'=>'mar',
+			'04'=>'abr',
+			'05'=>'mai',
+			'06'=>'jun',
+			'07'=>'jul',
+			'08'=>'ago',
+			'09'=>'set',
+			'10'=>'out',
+			'11'=>'nov',
+			'12'=>'dez'
+		];
+		$d = explode("-",$dt);
+		return strtoupper($mes[$d[1]])."/".$d[0];
+	}
+
     $titulo = array(
 		't' => '',
 		'n' => 'Novo',
@@ -74,76 +93,17 @@
 		left join motivos mt on a.motivo = mt.codigo
 		left join login tc on a.tecnico = tc.codigo
 		left join login f on a.funcionario = f.codigo
-	where (a.status != 'c') or (a.status = 'c' and a.data_fechamento >= NOW() - INTERVAL 30 DAY)
+	/*where (a.status != 'c') or (a.status = 'c' and a.data_fechamento >= NOW() - INTERVAL 30 DAY)*/
+	where a.status != 'c'
 		order by a.data_abertura asc";
 	$r = mysql_query($q);
 	// exit();
 	$TickDetalhe = [];
 	$TickResumo = [];
 
-	$Qt['novos'] = 0;
-	$Qt['pendentes'] = 0;
-	$Qt['concluidos'] = 0;
-	$Qt['parados'] = 0;
-
 	$visor = 0;
-
 	while($d = mysql_fetch_object($r)){
 
-
-		$Qt['novos'] = (($d->status == 'n')?($Qt['novos'] = ($Qt['novos'] + 1)):($Qt['novos']));
-		$Qt['pendentes'] = (($d->status == 'p')?($Qt['pendentes'] = ($Qt['pendentes'] + 1)):($Qt['pendentes']));
-		$Qt['concluidos'] = (($d->status == 'c')?($Qt['concluidos'] = ($Qt['concluidos'] + 1)):($Qt['concluidos']));
-		$Qt['parados'] = (($d->parada == 's' and $d->status != 'c')?($Qt['parados'] = ($Qt['parados'] + 1)):($Qt['parados']));
-
-		//Setores
-		$Rlt['setor']['nome'][$d->setor] = utf8_encode($d->setor_nome);
-		$Rlt['setor']['qt'][$d->setor] = ($Rlt['setor']['qt'][$d->setor] + 1);
-		$Rlt['setor']['tot'] = ($Rlt['setor']['tot'] + 1);
-
-		//UTMs
-		$Rlt['utm']['nome'][$d->utm] = utf8_encode($d->utm_nome);
-		$Rlt['utm']['qt'][$d->utm] = ($Rlt['utm']['qt'][$d->utm] + 1);
-		$Rlt['utm']['tot'] = ($Rlt['utm']['tot'] + 1);
-
-		// //Setores
-		// $Rlt['tipo_manutencao']['nome'][$d->tipo_manutencao] = utf8_encode($d->tipo_manutencao_nome);
-		// $Rlt['tipo_manutencao']['qt'][$d->tipo_manutencao] = ($Rlt['tipo_manutencao']['qt'][$d->tipo_manutencao] + 1);
-		// $Rlt['tipo_manutencao']['tot'] = ($Rlt['tipo_manutencao']['tot'] + 1);
-
-		//Motivos
-		$Rlt['motivo']['nome'][$d->motivo] = utf8_encode($d->motivo_nome);
-		$Rlt['motivo']['qt'][$d->motivo] = ($Rlt['motivo']['qt'][$d->motivo] + 1);
-		$Rlt['motivo']['tot'] = ($Rlt['motivo']['tot'] + 1);
-
-
-		//Peças
-		$Rlt['peca']['nome'][$d->peca] = utf8_encode($d->peca_nome);
-		$Rlt['peca']['qt'][$d->peca] = ($Rlt['peca']['qt'][$d->peca] + 1);
-		$Rlt['peca']['tot'] = ($Rlt['peca']['tot'] + 1);
-
-
-		//Modelos
-		$Rlt['modelo']['nome'][$d->modelo] = utf8_encode($d->modelo_nome);
-		$Rlt['modelo']['qt'][$d->modelo] = ($Rlt['modelo']['qt'][$d->modelo] + 1);
-		$Rlt['modelo']['tot'] = ($Rlt['modelo']['tot'] + 1);
-
-
-		//Códigos
-		$Rlt['codigos']['nome'][$d->codigos] = utf8_encode($d->codigos_nome);
-		$Rlt['codigos']['qt'][$d->codigos] = ($Rlt['codigos']['qt'][$d->codigos] + 1);
-		$Rlt['codigos']['tot'] = ($Rlt['codigos']['tot'] + 1);
-
-
-		//Time
-		$Rlt['time']['nome'][$d->time] = utf8_encode($d->time_nome);
-		$Rlt['time']['qt'][$d->time] = ($Rlt['time']['qt'][$d->time] + 1);
-		$Rlt['time']['tot'] = ($Rlt['time']['tot'] + 1);
-
-		//Paradas
-		if($d->parada == 's' and $d->status != 'c'){
-			$Rlt['paradas'][] = utf8_encode($d->maquina_nome);
-		}
 
 		if($d->status != 'c' and $visor < 7){
 
@@ -155,44 +115,75 @@
 
 			$Codigo[] = $d->codigo;
 
-		
+
 			$TickDetalhe[] = "
-					<div style='float:left; width:30%;'><b style='color:#a1a1a1; font-size:20px;''>Cadastrado ID:</b> <div class='detalhesTexto'>".str_pad($d->codigo, 8, "0", STR_PAD_LEFT)."</div></div>".
-					"<div style='float:left; width:45%;'>".((dataBr($d->data_abertura))?"<b style='color:#a1a1a1; font-size:20px;'>Data: <span style='color:red'>{$d->dias} dias atraso</span></b><div class='detalhesTexto'>".dataBr($d->data_abertura)."</div>":false)."</div>".
-					"<div style='float:left; width:25%;'>".(($d->status)?"<b style='color:#a1a1a1; font-size:20px;'>Situação:</b>
-						<div class='detalhesTexto' style='color:{$cor[$d->status]}; font-weight:bold;'>".$titulo[$d->status]."</div>":false)."</div>".
+					<div style='float:left; width:30%;'><b>Cadastrado ID:</b> <div >".str_pad($d->codigo, 8, "0", STR_PAD_LEFT)."</div></div>".
+					"<div style='float:left; width:50%;'>".((dataBr($d->data_abertura))?"<b>Data: <span style='color:red'>{$d->dias} dias atraso</span></b><div >".dataBr($d->data_abertura)."</div>":false)."</div>".
+					"<div style='float:left; width:20%;'>".(($d->status)?"<b>Situação:</b>
+						<div  style='color:{$cor[$d->status]}; font-weight:bold;'>".$titulo[$d->status]."</div>":false)."</div>".
 
-					"<div style='float:left; width:60%;'><b style='color:#a1a1a1; font-size:20px;''>Peça:</b> <div class='detalhesTexto'>".utf8_encode($d->peca_nome)."</div></div>".
-					"<div style='float:left; width:20%;'><b style='color:#a1a1a1; font-size:20px;'>Modelo:</b><div class='detalhesTexto'>".utf8_encode($d->modelo_nome)."</div></div>".
-					"<div style='float:left; width:20%;'><b style='color:#a1a1a1; font-size:20px;'>Código:</b><div class='detalhesTexto'>".utf8_encode($d->codigos_nome)."</div></div>".
+					"<div style='float:left; width:60%;'><b>Peça:</b> <div >".utf8_encode($d->peca_nome)."</div></div>".
+					"<div style='float:left; width:20%;'><b>Modelo:</b><div >".utf8_encode($d->modelo_nome)."</div></div>".
+					"<div style='float:left; width:20%;'><b>Código:</b><div >".utf8_encode($d->codigos_nome)."</div></div>".
 
+					"<div style='float:left; width:60%;'> <b>Setor:</b><div >".utf8_encode($d->setor_nome)." (".utf8_encode($d->utm_nome).")</div></div>".
+					"<div style='float:left; width:40%;'> <b>Máquina:<span style='color:".(($d->parada == 's')?'red':'#333').";'> (".$parada[$d->parada].")</span></b><div >".utf8_encode($d->maquina_nome)."</div></div>".
 
-					"<div style='float:left; width:50%;'> <b style='color:#a1a1a1; font-size:20px;'>Setor:</b><div class='detalhesTexto'>".utf8_encode($d->setor_nome)." (".utf8_encode($d->utm_nome).")</div></div>".
-					"<div style='float:left; width:50%;'> <b style='color:#a1a1a1; font-size:20px;'>Máquina:<span style='color:".(($d->parada == 's')?'red':'#333').";'> (".$parada[$d->parada].")</span></b><div class='detalhesTexto'>".utf8_encode($d->maquina_nome)."</div></div>".
-
-
-					"<div style='float:left; width:50%;'>".(($d->time_nome)?"<b style='color:#a1a1a1; font-size:20px;'>Time:</b><div class='detalhesTexto'>".utf8_encode($d->time_nome)."</div>":false)."</div>".
-					"<div style='float:left; width:50%;'>".(($d->motivo_nome)?"<b style='color:#a1a1a1; font-size:20px;'>Ocorrência:</b><div class='detalhesTexto'>".utf8_encode($d->motivo_nome)."</div>":false)."</div>".
+					"<div style='float:left; width:50%;'>".(($d->time_nome)?"<b>Time:</b><div >".utf8_encode($d->time_nome)."</div>":false)."</div>".
+					"<div style='float:left; width:50%;'>".(($d->motivo_nome)?"<b>Ocorrência:</b><div >".utf8_encode($d->motivo_nome)."</div>":false)."</div>".
 
 
-				//    "<div style='width:100%;'> <b style='color:#a1a1a1; font-size:10px;'>Tipo de Manutenção:</b><div>".utf8_encode($d->tipo_manutencao_nome)."</div></div>".
-				"<div style='width:100%;'>".(($d->problema)?"<b style='color:#a1a1a1; font-size:20px;'>Problema:</b><div class='detalhesTexto'>".str_replace("\n"," ",utf8_encode($d->problema))."</div>":false)."</div>".
+				//    "<div style='width:100%;'> <b style='color:#a1a1a1;'>Tipo de Manutenção:</b><div>".utf8_encode($d->tipo_manutencao_nome)."</div></div>".
+				"<div style='width:100%;'>".(($d->problema)?"<b>Problema:</b><div >".str_replace("\n"," ",utf8_encode($d->problema))."</div>":false)."</div>".
 
-				"<div style='float:left; width:50%;'>".(($d->funcionario)?"<b style='color:#a1a1a1; font-size:20px;'>Funcionário:</b><div class='detalhesTexto'>".utf8_encode($d->funcionario)."</div>":false)."</div>".
-				"<div style='float:left; width:50%;'>".(($d->tecnico)?"<b style='color:#a1a1a1; font-size:20px;'>Técnico:</b><div class='detalhesTexto'>".utf8_encode($d->tecnico)."</div>":false)."</div>".
+				"<div style='float:left; width:50%;'>".(($d->funcionario)?"<b>Funcionário:</b><div >".utf8_encode($d->funcionario)."</div>":false)."</div>".
+				"<div style='float:left; width:50%;'>".(($d->tecnico)?"<b>Técnico:</b><div >".utf8_encode($d->tecnico)."</div>":false)."</div>".
 
-				"<div style='width:100%;'>".(($d->observacao)?"<b style='color:#a1a1a1; font-size:20px;'>Observações:</b><div class='detalhesTexto'>".str_replace("\n"," ",$_POST['observacao'])."</div>":false)."</div><br>";
+				"<div style='width:100%;'>".(($d->observacao)?"<b>Observações:</b><div >".str_replace("\n"," ",$_POST['observacao'])."</div>":false)."</div><br>";
 
-			$TickResumo[] = "<div style='float:left; width:40%;'><b style='font-size:20px;''>Cadastrado ID:</b> <div style='font-size:23px;'>".str_pad($d->codigo, 8, "0", STR_PAD_LEFT)."</div></div>".
-							"<div style='float:left; width:60%;'>".(($d->status)?"<b style='font-size:20px;'>Situação:</b><div style='font-size:23px;'>".$titulo[$d->status]."</div>":false)."</div>".
-							"<div > <b style='font-size:20px;'>Setor:</b><div style='font-size:25px;'>".utf8_encode($d->setor_nome)." (".utf8_encode($d->utm_nome).")</div></div>".
-							"<div> <b style='font-size:20px;'>Máquina: (".$parada[$d->parada].")</b><div style='font-size:23px;'>".utf8_encode($d->maquina_nome)."</div></div>".
-							"<div > <b style='font-size:20px;'>Time:</b><div style='font-size:23px;'>".utf8_encode($d->time_nome)."</div></div>".
-							"<div> <b style='font-size:20px;'>Ocorrência:</b><div style='font-size:23px;'>".utf8_encode($d->motivo_nome)."</div></div>";
+			$TickResumo[] = "<div style='float:left; width:40%;'><b >Cadastrado ID:</b> <div>".str_pad($d->codigo, 8, "0", STR_PAD_LEFT)."</div></div>".
+							"<div style='float:left; width:60%;'>".(($d->status)?"<b >Situação:</b><div>".$titulo[$d->status]."</div>":false)."</div>".
+							"<div style='float:left; width:100%;'> <b >Setor:</b><div>".utf8_encode($d->setor_nome)." (".utf8_encode($d->utm_nome).")</div></div>".
+							"<div style='float:left; width:100%;'> <b >Máquina: (".$parada[$d->parada].")</b><div>".utf8_encode($d->maquina_nome)."</div></div>".
+							"<div style='float:left; width:100%;'> <b >Time:</b><div>".utf8_encode($d->time_nome)."</div></div>".
+							"<div style='float:left; width:100%;'> <b >Ocorrência:</b><div>".utf8_encode($d->motivo_nome)."</div></div>";
 		}
 
 
 	}
+
+
+
+//////////////////////////////////MES ATUAL/////////////////////////////////////
+
+
+$query = "select 
+(SELECT count(*) FROM `chamados` where data_abertura like '".date("Y-m")."%') as ch, 
+(SELECT count(*) FROM `chamados` where data_abertura like '".date("Y-m")."%' and status in ('n', 'p')) as pn, 
+(SELECT count(*) FROM `chamados` where data_abertura like '".date("Y-m")."%' and status in ('c')) as cl,
+(SELECT count(*) FROM `chamados` where data_abertura like '".date("Y-m")."%' and status not in ('c') and parada = 's') as pd";
+$result = mysql_query($query);
+$t = mysql_fetch_object($result);
+$Qt['novos'] = $t->ch;
+$Qt['pendentes'] = $t->pn;
+$Qt['concluidos'] = $t->cl;
+$Qt['parados'] = $t->pd; 
+
+//////////////////////////////////MES PASSADO/////////////////////////////////////
+
+
+$query = "select 
+(SELECT count(*) FROM `chamados` where data_abertura like '".$mes_passado."%') as ch, 
+(SELECT count(*) FROM `chamados` where data_abertura like '".$mes_passado."%' and status in ('n', 'p')) as pn, 
+(SELECT count(*) FROM `chamados` where data_abertura like '".$mes_passado."%' and status in ('c')) as cl,
+(SELECT count(*) FROM `chamados` where data_abertura like '".$mes_passado."%' and status not in ('c') and parada = 's') as pd";
+$result = mysql_query($query);
+$t = mysql_fetch_object($result);
+$MP['novos'] = $t->ch;
+$MP['pendentes'] = $t->pn;
+$MP['concluidos'] = $t->cl;
+$MP['parados'] = $t->pd; 
+
 
 ?>
 <!DOCTYPE html>
@@ -411,51 +402,26 @@
 	<div class="row">
 		<div class="col">
 			<div class="Qt" style="background-color:blue">
-				<span>Chamados</span><h1><?=str_pad(trim($Qt['novos']) +  trim($Qt['pendentes']) + trim($Qt['parados']) + trim($Qt['concluidos']), 4 , '0' , STR_PAD_LEFT)?></h1>
+				<div>Chamados - CH </div><h1><?=str_pad(trim($Qt['novos']) , 4 , '0' , STR_PAD_LEFT)?></h1>
 			</div>
 		</div>
 		<div class="col">
 			<div class="Qt" style="background-color:orange">
-				<span>Pendentes</span><h1><?=str_pad(trim($Qt['novos']) +  trim($Qt['pendentes']) + trim($Qt['parados']) , 4 , '0' , STR_PAD_LEFT)?></h1>
+				<div>Pendentes - PD </div><h1><?=str_pad(trim($Qt['pendentes']) , 4 , '0' , STR_PAD_LEFT)?></h1>
 			</div>
 		</div>
 		<div class="col">
 			<div class="Qt" style="background-color:red">
-				<span>Paradas</span><h1><?=str_pad(trim($Qt['parados']) , 4 , '0' , STR_PAD_LEFT)?></h1>
+				<div>Paradas - PR </div><h1><?=str_pad(trim($Qt['parados']) , 4 , '0' , STR_PAD_LEFT)?></h1>
 			</div>
 		</div>
 		<div class="col">
 			<div class="Qt" style="background-color:green">
-				<span>Concluído (últimas 30Dias)</span><h1><?=str_pad(trim($Qt['concluidos']) , 4 , '0' , STR_PAD_LEFT)?></h1>
+				<div>Concluído - CL (De <?=periodo(date("Y-m"))?>)</div><h1><?=str_pad(trim($Qt['concluidos']) , 4 , '0' , STR_PAD_LEFT)?></h1>
 			</div>
 		</div>
 	</div>
-	<?php
-	if($Rlt['paradasXXX']){
-	?>
-	<div class="row mt-3">
-		<div class="col">
-			<div class="lista_maquinas">
-				<h4>Máquinas Paradas</h4>
-				<div class="lista_maquinas_paradas">
-				<?php
 
-					foreach($Rlt['paradas'] as $ind => $maq){
-						// for($i=0;$i<20;$i++){
-				?>
-					<div style="margin:5px; border-radius:5px; background-color:red; padding:10px; font-size:29px;"><?=$maq?></div>
-				<?php
-						// }
-					}
-
-				?>
-				</div>
-			</div>
-		</div>
-	</div>
-	<?php
-	}
-	?>
 	<div class="row mt-3">
 		<!-- <div class="col">
 			<div class="graficos">
