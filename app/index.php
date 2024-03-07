ffa43114f5edae72dc7a1a1da0f5b97082d0d2e3
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-07 21:42:39 +0200

    Atualizações

--- app/index.php
+++ app/index.php
@@ -138,8 +138,8 @@
 				//    "<div style='width:100%;'> <b style='color:#a1a1a1;'>Tipo de Manutenção:</b><div>".utf8_encode($d->tipo_manutencao_nome)."</div></div>".
 				"<div style='width:100%;'>".(($d->problema)?"<b>Problema:</b><div class='detalhesTexto' style='overflow: hidden; display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 2;' >".str_replace("\n"," ",utf8_encode($d->problema))."</div>":false)."</div>".
 
-				"<div style='float:left; width:100%;'>".(($d->funcionario)?"<b>Funcionário:</b><div class='detalhesTexto'>".utf8_encode($d->funcionario)."</div>":false)."</div>".
-				"<div style='float:left; width:100%;'>".(($d->tecnico)?"<b>Técnico:</b><div class='detalhesTexto'>".utf8_encode($d->tecnico)."</div>":false)."</div>".
+				"<div style='float:left; width:50%;'>".(($d->funcionario)?"<b>Funcionário:</b><div class='detalhesTexto' style='overflow: hidden; display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 1;'>".utf8_encode($d->funcionario)."</div>":false)."</div>".
+				"<div style='float:left; width:100%;'>".(($d->tecnico)?"<b>Técnico:</b><div class='detalhesTexto' style='overflow: hidden; display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 1;'>".utf8_encode($d->tecnico)."</div>":false)."</div>".
 
 				"<div style='width:100%;'>".(($d->observacao)?"<b>Observações:</b><div class='detalhesTexto'>".str_replace("\n"," ",$_POST['observacao'])."</div>":false)."</div><br>";
 
