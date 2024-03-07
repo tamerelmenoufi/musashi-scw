106628dc393464d8ff28b76f2b8957dd74264549
tamerelmenoufi <tamer@mohatron.com.br>
2024-03-07 10:53:19 -0400

    Atualizações

--- tv/index.php
+++ tv/index.php
@@ -786,8 +786,8 @@ $MP['parados'] = $t->pd;
 </table>
 </div>
 
-<div style="position:fixed; bottom:10px; left:610px; right:15px; border:solid 2px #fff">
-	<span style="font-size:15px; font-weight:bold; padding-left:20px;">Histórico do mês anterior</span>
+<div style="position:fixed; bottom:10px; left:610px; right:15px; border-top:solid 2px #fff">
+	<span style="font-size:15px; font-weight:bold; padding-left:10px;">Histórico do mês anterior</span>
 	<div class="row">
 		<div class="col">
 			<div class="Qt" style="background-color:blue">
