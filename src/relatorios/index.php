522af2e0aa108d6cfb50133e39585a06e7ac6b23
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-17 01:31:17 +0200

    Atualizações

--- src/relatorios/index.php
+++ src/relatorios/index.php
@@ -121,8 +121,8 @@
                     a.*,
                     b.utm,
                     c.nome as utm_nome,
-                    b.nome as setor_nome,
-                    t.nome as time_nome,
+                    b.sigla as setor_nome,
+                    t.sigla as time_nome,
                     m.nome as maquina_nome
                 from chamados a 
                 left join setores b on a.setor = b.codigo 
@@ -214,7 +214,7 @@
     foreach($relatorio['utm'] as $i => $v){
         
         // if($i){
-        $grafico_utm['legenda'][] = strtoupper(substr($i,0,2)).str_pad($j, 2, "0", STR_PAD_LEFT);
+        $grafico_utm['legenda'][] = $i; //strtoupper(substr($i,0,2)).str_pad($j, 2, "0", STR_PAD_LEFT);
         $grafico_utm['nome'][] = $v['nome']*1;
         $grafico_utm['pendente'][] = $v['pendente']*1;
         $grafico_utm['concluido'][] = $v['concluido']*1;
@@ -261,7 +261,7 @@
     arsort($relatorio['setor']);
     $j = 1;
     foreach($relatorio['setor'] as $i => $v){
-        $grafico_setor['legenda'][] = strtoupper(substr($i,0,2)).str_pad($j, 2, "0", STR_PAD_LEFT);
+        $grafico_setor['legenda'][] = $i; //strtoupper(substr($i,0,2)).str_pad($j, 2, "0", STR_PAD_LEFT);
         $grafico_setor['nome'][] = $v['nome']*1;
         $grafico_setor['pendente'][] = $v['pendente']*1;
         $grafico_setor['concluido'][] = $v['concluido']*1;
@@ -269,7 +269,7 @@
         $grafico_setor['producao'][] = $v['producao']*1;
 ?>
         <tr>
-            <td><?=strtoupper(substr($i,0,2)).str_pad($j, 2, "0", STR_PAD_LEFT)?></td>
+            <td><?=$i/*strtoupper(substr($i,0,2)).str_pad($j, 2, "0", STR_PAD_LEFT)*/?></td>
             <td><?=$i?></td>
             <td><?=$v['nome']*1?></td>
             <td><?=$v['concluido']*1?></td>
@@ -285,7 +285,7 @@ $j++;
     </tbody>
 </table>
 
-<!-- <canvas id="grafico_setor" style="margin-top:30px;"></canvas> -->
+<canvas id="grafico_setor" style="margin-top:30px;"></canvas>
 
 
 
@@ -309,7 +309,7 @@ $j++;
     arsort($relatorio['time']);
     $j = 1;
     foreach($relatorio['time'] as $i => $v){
-        $grafico_time['legenda'][] = strtoupper(substr($i,0,2)).str_pad($j, 2, "0", STR_PAD_LEFT);
+        $grafico_time['legenda'][] = $i; //strtoupper(substr($i,0,2)).str_pad($j, 2, "0", STR_PAD_LEFT);
         $grafico_time['nome'][] = $v['nome']*1;
         $grafico_time['pendente'][] = $v['pendente']*1;
         $grafico_time['concluido'][] = $v['concluido']*1;
@@ -317,7 +317,7 @@ $j++;
         $grafico_time['producao'][] = $v['producao']*1;
 ?>
         <tr>
-            <td><?=strtoupper(substr($i,0,2)).str_pad($j, 2, "0", STR_PAD_LEFT)?></td>
+            <td><?=$i/*strtoupper(substr($i,0,2)).str_pad($j, 2, "0", STR_PAD_LEFT)*/?></td>
             <td><?=$i?></td>
             <td><?=$v['nome']*1?></td>
             <td><?=$v['concluido']*1?></td>
@@ -333,7 +333,7 @@ $j++;
     </tbody>
 </table>
 
-<!-- <canvas id="grafico_time" style="margin-top:30px;"></canvas> -->
+<canvas id="grafico_time" style="margin-top:30px;"></canvas>
 
 
 
@@ -357,7 +357,7 @@ $j++;
     arsort($relatorio['maquina']);
     $j = 1;
     foreach($relatorio['maquina'] as $i => $v){
-        $grafico_maquina['legenda'][] = strtoupper(substr($i,0,2)).str_pad($j, 2, "0", STR_PAD_LEFT);
+        $grafico_maquina['legenda'][] = $i; //strtoupper(substr($i,0,2)).str_pad($j, 2, "0", STR_PAD_LEFT);
         $grafico_maquina['nome'][] = $v['nome']*1;
         $grafico_maquina['pendente'][] = $v['pendente']*1;
         $grafico_maquina['concluido'][] = $v['concluido']*1;
@@ -365,7 +365,7 @@ $j++;
         $grafico_maquina['producao'][] = $v['producao']*1;
 ?>
         <tr>
-            <td><?=strtoupper(substr($i,0,2)).str_pad($j, 2, "0", STR_PAD_LEFT)?></td>
+            <td><?=$i/*strtoupper(substr($i,0,2)).str_pad($j, 2, "0", STR_PAD_LEFT)*/?></td>
             <td><?=$i?></td>
             <td><?=$v['nome']*1?></td>
             <td><?=$v['concluido']*1?></td>
@@ -381,7 +381,7 @@ $j++;
     </tbody>
 </table>
 
-<!-- <canvas id="grafico_maquina" style="margin-top:30px;"></canvas> -->
+<canvas id="grafico_maquina" style="margin-top:30px;"></canvas>
 
 
 
@@ -390,7 +390,7 @@ $j++;
 
     ///////////////////////// Grafico ////////////////////////////////////////////////////////////
 
-
+/*
     new Chart("grafico_setor", {
         type: "horizontalBar",
         data: {
@@ -549,4 +549,207 @@ new Chart("grafico_utm", {
 
 
     })
+    //*/
+
+
+      ///////////////////////// Grafico ////////////////////////////////////////////////////////////
+
+
+  new Chart("grafico_setor", {
+      type: "bar", //horizontalBar
+      data: {
+          labels: ['<?=implode("', '", $grafico_setor['legenda'])?>'],
+          datasets: [{
+          label: 'Geral',
+          data: [<?=implode(", ", $grafico_setor['nome'])?>],
+          borderColor: "blue",
+          backgroundColor:"rgb(2, 62, 198, 0.7)",
+          fill: false
+          },{
+          label: 'Concluidos',
+          data: [<?=implode(", ", $grafico_setor['concluido'])?>],
+          borderColor: "green",
+          backgroundColor:"rgb(1, 174, 50, 0.7)",
+          fill: false
+          },{
+          label: 'Pendentes',
+          data: [<?=implode(", ", $grafico_setor['pendente'])?>],
+          borderColor: "gray",
+          backgroundColor:"rgb(116, 116, 116, 0.7)",
+          fill: false
+          },{
+          label: 'Máquinas Paradas',
+          data: [<?=implode(", ", $grafico_setor['parada'])?>],
+          borderColor: "red",
+          backgroundColor:"rgb(200, 3, 54, 0.7)",
+          fill: false
+          },{
+          label: 'Máquinas Em Produção',
+          data: [<?=implode(", ", $grafico_setor['producao'])?>],
+          borderColor: "orange",
+          backgroundColor:"rgb(247, 152, 2, 0.7)",
+          fill: false
+          }]
+      },
+      options: {
+          legend: {display: false},
+          title: {
+              display: true,
+              text: "Gráfico de Representação dos Setores",
+              fontSize: 16
+          }
+      }
+  });
+  
+
+
+///////////////////////// Grafico ////////////////////////////////////////////////////////////
+
+
+new Chart("grafico_utm", {
+  type: "bar", //horizontalBar
+  data: {
+      labels: ['<?=implode("', '", $grafico_utm['legenda'])?>'],
+      datasets: [{
+      label: 'Geral',
+      data: [<?=implode(", ", $grafico_utm['nome'])?>],
+      borderColor: "blue",
+      backgroundColor:"rgb(2, 62, 198, 0.7)",
+      fill: false
+      },{
+      label: 'Concluidos',
+      data: [<?=implode(", ", $grafico_utm['concluido'])?>],
+      borderColor: "green",
+      backgroundColor:"rgb(1, 174, 50, 0.7)",
+      fill: false
+      },{
+      label: 'Pendentes',
+      data: [<?=implode(", ", $grafico_utm['pendente'])?>],
+      borderColor: "gray",
+      backgroundColor:"rgb(116, 116, 116, 0.7)",
+      fill: false
+      },{
+      label: 'Máquinas Paradas',
+      data: [<?=implode(", ", $grafico_utm['parada'])?>],
+      borderColor: "red",
+      backgroundColor:"rgb(200, 3, 54, 0.7)",
+      fill: false
+      },{
+      label: 'Máquinas Em Produção',
+      data: [<?=implode(", ", $grafico_utm['producao'])?>],
+      borderColor: "orange",
+      backgroundColor:"rgb(247, 152, 2, 0.7)",
+      fill: false
+      }]
+  },
+  options: {
+      legend: {display: false},
+      title: {
+          display: true,
+          text: "Gráfico de Representação das UTM's",
+          fontSize: 16
+      }
+  }
+});
+
+
+
+  ///////////////////////// Grafico ////////////////////////////////////////////////////////////
+
+
+  new Chart("grafico_time", {
+      type: "bar", //horizontalBar
+      data: {
+          labels: ['<?=implode("', '", $grafico_time['legenda'])?>'],
+          datasets: [{
+          label: 'Geral',
+          data: [<?=implode(", ", $grafico_time['nome'])?>],
+          borderColor: "blue",
+          backgroundColor:"rgb(2, 62, 198, 0.7)",
+          fill: false
+          },{
+          label: 'Concluidos',
+          data: [<?=implode(", ", $grafico_time['concluido'])?>],
+          borderColor: "green",
+          backgroundColor:"rgb(1, 174, 50, 0.7)",
+          fill: false
+          },{
+          label: 'Pendentes',
+          data: [<?=implode(", ", $grafico_time['pendente'])?>],
+          borderColor: "gray",
+          backgroundColor:"rgb(116, 116, 116, 0.7)",
+          fill: false
+          },{
+          label: 'Máquinas Paradas',
+          data: [<?=implode(", ", $grafico_time['parada'])?>],
+          borderColor: "red",
+          backgroundColor:"rgb(200, 3, 54, 0.7)",
+          fill: false
+          },{
+          label: 'Máquinas Em Produção',
+          data: [<?=implode(", ", $grafico_time['producao'])?>],
+          borderColor: "orange",
+          backgroundColor:"rgb(247, 152, 2, 0.7)",
+          fill: false
+          }]
+      },
+      options: {
+          legend: {display: false},
+          title: {
+              display: true,
+              text: "Gráfico de Representação dos Times",
+              fontSize: 16
+          }
+      }
+  });
+  
+  ///////////////////////// Grafico ////////////////////////////////////////////////////////////
+
+
+  new Chart("grafico_maquina", {
+      type: "bar", //horizontalBar
+      data: {
+          labels: ['<?=implode("', '", $grafico_maquina['legenda'])?>'],
+          datasets: [{
+          label: 'Geral',
+          data: [<?=implode(", ", $grafico_maquina['nome'])?>],
+          borderColor: "blue",
+          backgroundColor:"rgb(2, 62, 198, 0.7)",
+          fill: false
+          },{
+          label: 'Concluidos',
+          data: [<?=implode(", ", $grafico_maquina['concluido'])?>],
+          borderColor: "green",
+          backgroundColor:"rgb(1, 174, 50, 0.7)",
+          fill: false
+          },{
+          label: 'Pendentes',
+          data: [<?=implode(", ", $grafico_maquina['pendente'])?>],
+          borderColor: "gray",
+          backgroundColor:"rgb(116, 116, 116, 0.7)",
+          fill: false
+          },{
+          label: 'Máquinas Paradas',
+          data: [<?=implode(", ", $grafico_maquina['parada'])?>],
+          borderColor: "red",
+          backgroundColor:"rgb(200, 3, 54, 0.7)",
+          fill: false
+          },{
+          label: 'Máquinas Em Produção',
+          data: [<?=implode(", ", $grafico_maquina['producao'])?>],
+          borderColor: "orange",
+          backgroundColor:"rgb(247, 152, 2, 0.7)",
+          fill: false
+          }]
+      },
+      options: {
+          legend: {display: false},
+          title: {
+              display: true,
+              text: "Gráfico de Representação dos Máquinas",
+              fontSize: 16
+          }
+      }
+  });
+  
 </script>
