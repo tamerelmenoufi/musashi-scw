0c77a8ff4eb63ca118249d203dfd10c5bc6ddc54
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-05-14 16:03:10 +0300

    Atualizações

---
+++ app2/index.php
@@ -0,0 +1,29 @@
+<?php
+	$home = true;
+	include('../includes/includes.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SISTEMA - SCW/TV</title>
+	<?php include('../lib/header.php'); ?>
+	<style>
+        body{
+            padding:0;
+            margin:0;
+            width:100%;
+            height:100%;
+            border:solid 1px red;
+        }
+	</style>
+</head>
+<body>
+
+<script type="text/javascript">
+
+	$(function(){
+
+	})
+</script>
+</body>
+</html>
