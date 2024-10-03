ce388e05cd397c6b9fe71243f097e4651094a4b3
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-10-03 20:47:28 +0300

    Atualizações

--- tv2/index.php
+++ tv2/index.php
@@ -37,7 +37,7 @@
 <script type="text/javascript">
 
 	Carregando = (opc) => { $("#Carregando").css("display",(opc?opc:'block')) }
-
+	/////////////////////////////////
 	$(function(){
 		Carregando('none')
 
